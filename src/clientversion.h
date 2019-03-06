6a9e60022bd644b08d1173536924906d0e721c35
avymantech <38569279+avymantech@users.noreply.github.com>
2019-03-05 21:04:17 -0300

    Version and Copyright year update

--- src/clientversion.h
+++ src/clientversion.h
@@ -27,7 +27,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2017
+#define COPYRIGHT_YEAR 2019
 
 #endif //HAVE_CONFIG_H
 
@@ -39,7 +39,7 @@
 
 // Modify version number here!!!
 #define CLIENT_VERSION_MAJOR 2
-#define CLIENT_VERSION_MINOR 1
+#define CLIENT_VERSION_MINOR 2
 #define CLIENT_VERSION_REVISION 1
 #define CLIENT_VERSION_BUILD 0
 
